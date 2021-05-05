d7b42975852bbb6c80af24767bb1915beff6646c
fewensa <fewensa@protonmail.com>
2021-05-05 10:18:47 +0800

    build

--- .maintain/build-tags-linux-x86.sh
+++ .maintain/build-tags-linux-x86.sh
@@ -11,15 +11,22 @@ VERSION=$1
 
 git clone https://github.com/tdlib/td.git
 cd td
-
 git checkout -b tags/${VERSION} ${VERSION}
 
 rm -rf build
 mkdir build
 
-cd td/build
+cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX:PATH=../tdlib ..
 cmake --build . --target install
 
 
+ls
+
+PATH_OUT=${BIN_PATH}/x86_64/linux/${VERSION}
+mkdir -p ${PATH_OUT}
+
+cp ${BIN_PATH}/td/tdlib/lib/*libtdjson.so* ${PATH_OUT}
+
+ls ${PATH_OUT}
 
